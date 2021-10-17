064b32bee42cc533476954686540427c9207b1a7
Toshiyuki Hara <thara0402@gmail.com>
2021-10-17 20:32:42 +0900

    debug.

--- WebApplication/Startup.cs
+++ WebApplication/Startup.cs
@@ -85,10 +85,11 @@ namespace WebApplication
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+            app.UseDeveloperExceptionPage();    // dbg
 
             app.UseSwagger();
             app.UseSwaggerUI(c =>
