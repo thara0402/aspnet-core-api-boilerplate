151af8fe8efaec55bb93e06f8e90aba4e2b16bc1
Toshiyuki Hara <thara0402@gmail.com>
2021-10-17 19:42:05 +0900

    debug.

--- WebApplication/Startup.cs
+++ WebApplication/Startup.cs
@@ -58,22 +58,22 @@ namespace WebApplication
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApplication", Version = "v1" });
 
-                //c.TagActionsBy(api =>
-                //{
-                //    if (api.GroupName != null)
-                //    {
-                //        return new[] { api.GroupName };
-                //    }
+                c.TagActionsBy(api =>
+                {
+                    if (api.GroupName != null)
+                    {
+                        return new[] { api.GroupName };
+                    }
 
-                //    var controllerActionDescriptor = api.ActionDescriptor as ControllerActionDescriptor;
-                //    if (controllerActionDescriptor != null)
-                //    {
-                //        return new[] { controllerActionDescriptor.ControllerName };
-                //    }
+                    var controllerActionDescriptor = api.ActionDescriptor as ControllerActionDescriptor;
+                    if (controllerActionDescriptor != null)
+                    {
+                        return new[] { controllerActionDescriptor.ControllerName };
+                    }
 
-                //    throw new InvalidOperationException("Unable to determine tag for endpoint.");
-                //});
-                //c.DocInclusionPredicate((name, api) => true);
+                    throw new InvalidOperationException("Unable to determine tag for endpoint.");
+                });
+                c.DocInclusionPredicate((name, api) => true);
             });
         }
 
